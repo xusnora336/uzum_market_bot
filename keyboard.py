eba5d45befa2fcc13a502528041a5d9088238128
karri <husnoraxamitova@gmail.com>
2025-06-05 09:56:13 +0500

    menyu batton qoshldi

--- keyboard.py
+++ keyboard.py
@@ -1,8 +1,9 @@
 from aiogram.types import  KeyboardButton, ReplyKeyboardMarkup,WebAppInfo
 
-
-
-btn_keyboard=ReplyKeyboardMarkup(
+btn=ReplyKeyboardMarkup(
     keyboard=[
-        [KeyboardButton(text="Kompaniya xaqida"),
-         KeyboardButton(text="menyu",web_app=WebAppInfo(url="")])
+        [KeyboardButton(text="Kompaniya haqida"),
+         KeyboardButton(text="Menyu",web_app=WebAppInfo(url="https://uzum.uz/")),],
+        [KeyboardButton(text="Kontakt/Manzil"),],
+    ],resize_keyboard=True,
+)
