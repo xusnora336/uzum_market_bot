67d86ed795bb50c00f760c14bb4b0b1e3be0fa38
karri <husnoraxamitova@gmail.com>
2025-06-05 10:05:36 +0500

    menyu batton qoshldi

--- bot.py
+++ bot.py
@@ -4,22 +4,26 @@ from os import getenv
 from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
 from aiogram.types import Message
+from dotenv import load_dotenv
 
-TOKEN = getenv("BOT_TOKEN")
-
+import keyboard
+from keyboard import btn
+BOT_TOKEN = getenv("BOT_TOKEN")
+load_dotenv()
 dp = Dispatcher()
 
 
 @dp.message(Command("start"))
 async def command_start_handler(message: Message) -> None:
-    await message.answer("Uzum market botiga xush kelibsz")
+    await message.answer("Uzum market botiga xush kelibsz",reply_markup=btn)
 
 
 # Run the bot
 async def main() -> None:
-    bot = Bot(token=TOKEN)
+    bot = Bot(token=BOT_TOKEN)
     await dp.start_polling(bot)
 
-
 if __name__ == "__main__":
+    print("Starting bot...")
+
     asyncio.run(main())
