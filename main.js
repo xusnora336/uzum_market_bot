c28b44f04398f8c79ec90a52eee3b0615344f99c
User <muso17091@gmail.com>
2025-06-21 10:29:51 +0500

    main.css

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
-let tg = Window.Telegram.WebApp;
+let tg = window.Telegram.WebApp;
 tg.expand();
+
+// Функция для проверки общего количества товаров
+function getTotalQuantity() {
+  const qtyDivs = document.querySelectorAll('.qty-number');
+  let total = 0;
+  qtyDivs.forEach(div => {
+    total += parseInt(div.innerText);
+  });
+  return total;
+}
+
+// Функция для обновления состояния MainButton
+function updateMainButton() {
+  const totalQuantity = getTotalQuantity();
+  if (totalQuantity > 0) {
+    tg.MainButton.show();
+  } else {
+    tg.MainButton.hide();
+  }
+}
 
 function increase(btn) {
   const qtyDiv = btn.parentElement.querySelector('.qty-number');
@@ -13,28 +33,12 @@ function decrease(btn) {
   let value = parseInt(qtyDiv.innerText);
   if (value > 0) {
     qtyDiv.innerText = value - 1;
-    updateMainButton();
   }
-}
-
-function updateMainButton() {
-  const qtyNumbers = document.querySelectorAll('.qty-number');
-  let totalItems = 0;
-
-  qtyNumbers.forEach(qty => {
-    totalItems += parseInt(qty.innerText);
-  });
-
-  if (totalItems > 0) {
-    tg.MainButton.show();
-  } else {
-    tg.MainButton.hide();
-  }
+  updateMainButton();
 }
 
 function filterProducts(category) {
   const cards = document.querySelectorAll('.product-card');
-
   cards.forEach(card => {
     const cardCategory = card.getAttribute('data-category');
     if (category === 'all') {
