16fb517cde99287bbb1c86f3bd69c9942fecdcae
User <muso17091@gmail.com>
2025-06-21 10:13:19 +0500

    main.css

--- main.js
+++ main.js
@@ -1,11 +1,31 @@
-let tg = Window.Telegram.WebApp;
+let tg = window.Telegram.WebApp;
 tg.expand();
+
+// Функция для проверки общего количества товаров
+function getTotalQuantity() {
+  const qtyDivs = document.querySelectorAll('.qty-number');
+  let total = 0;
+  qtyDivs.forEach(div => {
+    total += parseInt(div.innerText);
+  });
+  return total;
+}
+
+// Функция для обновления состояния MainButton
+function updateMainButton() {
+  const totalQuantity = getTotalQuantity();
+  if (totalQuantity > 0) {
+    tg.MainButton.show();
+  } else {
+    tg.MainButton.hide();
+  }
+}
 
 function increase(btn) {
   const qtyDiv = btn.parentElement.querySelector('.qty-number');
   let value = parseInt(qtyDiv.innerText);
   qtyDiv.innerText = value + 1;
-  tg.MainButton.show();
+  updateMainButton();
 }
 
 function decrease(btn) {
@@ -14,11 +34,11 @@ function decrease(btn) {
   if (value > 0) {
     qtyDiv.innerText = value - 1;
   }
+  updateMainButton();
 }
 
 function filterProducts(category) {
   const cards = document.querySelectorAll('.product-card');
-
   cards.forEach(card => {
     const cardCategory = card.getAttribute('data-category');
     if (category === 'all') {
