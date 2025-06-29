3df06bfb4a237e44cea3bb5277fa3edfe428ee7c
karri <husnoraxamitova@gmail.com>
2025-06-29 12:44:40 +0500

    added session bot

--- bot.py
+++ bot.py
@@ -7,8 +7,16 @@ from aiogram.types import Message
 from dotenv import load_dotenv
 from keyboard import btn
 from handlers import router
+from aiogram.client.session.aiohttp import AiohttpSession
 load_dotenv()
+
+
+
+
 BOT_TOKEN = getenv("BOT_TOKEN")
+
+session=AiohttpSession(proxy="http://proxy.server:3128")
+
 dp = Dispatcher()
 dp.include_router(router)
 
@@ -19,7 +27,8 @@ async def command_start_handler(message: Message) -> None:
 
 # Run the bot
 async def main() -> None:
-    bot = Bot(token=BOT_TOKEN)
+    # bot = Bot(token=BOT_TOKEN)
+    bot=Bot(token=BOT_TOKEN,session=session)
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
